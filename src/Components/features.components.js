82f6a10bf348a4de3a8eadefe4ecd4630af5f8c5
Aman Singh <singh.aman200226@gmail.com>
2022-10-08 21:15:39 +0530

    component added

---
+++ src/Components/features.components.js
@@ -0,0 +1,43 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle, faLaptop, faRibbon, faUserGroup } from '@fortawesome/free-solid-svg-icons'
+
+const Features = () => {
+    return ( 
+        <div className="feature-container">
+            <div className="feature">
+                <div className="feature-icon">
+                <FontAwesomeIcon icon={faUserGroup} />
+                </div>
+                <div className="feature-name">
+                    One to one interaction
+                </div>
+            </div>
+            <div className="feature">
+                <div className="feature-icon">
+                    <FontAwesomeIcon icon={faCircle} />
+                </div>
+                <div className="feature-name">
+                    Explore our courses
+                </div>
+            </div>
+            <div className="feature">
+                <div className="feature-icon">
+                    <FontAwesomeIcon icon={faLaptop} />
+                </div>
+                <div className="feature-name">
+                    Lifetime access on mobile and desktop
+                </div>
+            </div>
+            <div className="feature">
+                <div className="feature-icon">
+                    <FontAwesomeIcon icon={faRibbon} />
+                </div>
+                <div className="feature-name">
+                    Become a certified Developer
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Features;
